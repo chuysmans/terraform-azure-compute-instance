020f52a25291267e0fd40cb7ba41fa7bde805ca2
Cam <cameron.huysmans@gmail.com>
2018-08-31 15:20:33 +1000

    deleted the RG

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
 # Create a resource group if it doesn’t exist
-resource "azurerm_resource_group" "myfirstrg" {
-  name     = "${var.name_prefix}-RG"
-  location = "${var.resource_group_location}"
+#resource "azurerm_resource_group" "myfirstrg" {
+#  name     = "${var.name_prefix}-RG"
+#  location = "${var.resource_group_location}"
 
-  tags {
-    environment = "${var.environment_tag}"
-    ttl         = "${var.ttl_tag}"
-    owner       = "${var.owner_tag}"
-  }
-}
+#  tags {
+#    environment = "${var.environment_tag}"
+#    ttl         = "${var.ttl_tag}"
+#    owner       = "${var.owner_tag}"
+#  }
+#}
 
 module "windowsserver" {
   source              = "Azure/compute/azurerm"
